1693af25ec11f815974358ee0da41d0e3b11ca37
Shreeti Patel <shreetipatel06@gmail.com>
2023-04-11 20:04:26 -0400

    Album fxml created, Album Controller Created

---
+++ src/controllers/AlbumController.java
@@ -0,0 +1,154 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class AlbumController {
+
+    @FXML
+    private Button addPhotoButton;
+
+    @FXML
+    private Button addTagButton;
+
+    @FXML
+    private TextField addTagKeyField;
+
+    @FXML
+    private TextField addTagValueField;
+
+    @FXML
+    private Label albumNameLable;
+
+    @FXML
+    private AnchorPane albumPane;
+
+    @FXML
+    private Button copyPhotoButton;
+
+    @FXML
+    private Button deleteTagButton;
+
+    @FXML
+    private ChoiceBox<?> destinationAlbumChoiceBox;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button movePhotoButton;
+
+    @FXML
+    private TextField newCaptionField;
+
+    @FXML
+    private Button nextPhotoButton;
+
+    @FXML
+    private ListView<?> photoCaptionListView;
+
+    @FXML
+    private Pane photoDisplayPane;
+
+    @FXML
+    private ListView<?> photoThumbListView;
+
+    @FXML
+    private Button prevPhotoButton;
+
+    @FXML
+    private Button removePhotoButton;
+
+    @FXML
+    private Button searchPhotoButton;
+
+    @FXML
+    private ChoiceBox<?> selectTagToDeleteChoiceBox;
+
+    @FXML
+    private Button updateCaptionButton;
+
+    @FXML
+    private Button viewAlbumButton;
+
+    @FXML
+    void getNewCaption(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getTagKey(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getTagValue(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAddPhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAddTagButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCopyPhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDeleteTagButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogOutButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMovePhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNextPhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handlePrevPhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRemovePhotoButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSearchPhotosButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleUpdateCaptionButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleViewAlbumButtonClick(ActionEvent event) {
+
+    }
+
+}
